ef3cce3ed618e3fbad265a845129d149aabd0eaf
Dan C <djc5627@gmail.com>
2022-04-02 15:47:20 -0400

    Create font assets. Add Task manager. Add interactable receiver. Add interactables to scene.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace BML.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private LayerMask interactMask;
+        [SerializeField] private Transform mainCamera;
+        [SerializeField] private float interactDistance = 10f;
+
+        public void OnInteract()
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(mainCamera.position, mainCamera.forward, out hit, interactDistance, interactMask))
+            {
+                //Debug.Log($"Hit {hit.collider.gameObject.name}");
+
+                InteractionReceiver interactionReceiver = hit.collider.GetComponent<InteractionReceiver>();
+                if (interactionReceiver == null) return;
+                
+                interactionReceiver.ReceiveInteraction();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TaskManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using BML.ScriptableObjectCore.Scripts.Events;
+using BML.ScriptableObjectCore.Scripts.Variables;
+using TMPro;
+using UnityEngine;
+
+namespace BML.Scripts
+{
+    public class TaskManager : MonoBehaviour
+    {
+        [SerializeField] private GameEvent OnGrabBox;
+        [SerializeField] private GameEvent OnGrabProduct;
+        [SerializeField] private GameEvent OnDepositBox;
+        [SerializeField] private IntReference BoxesDepositedCount;
+        [SerializeField] private TMP_Text CurrentTaskText;
+        [SerializeField] private String GrabBoxText = "Grab a Box";
+        [SerializeField] private String GrabProductText = "Grab the Product";
+        [SerializeField] private String DepositBoxText = "Deposit the Box";
+
+        private Task CurrentTask = Task.GrabBox;
+        
+        enum Task
+        {
+            GrabBox,
+            GrabProduct,
+            DepositBox
+        }
+
+        private void Awake()
+        {
+            OnGrabBox.Subscribe(GrabBox);
+            OnGrabProduct.Subscribe(GrabProduct);
+            OnDepositBox.Subscribe(DepositBox);
+        }
+
+        private void Start()
+        {
+            CurrentTaskText.text = GrabBoxText;
+        }
+
+        private void GrabBox()
+        {
+            if (CurrentTask == Task.GrabBox)
+            {
+                CurrentTask = Task.GrabProduct;
+                CurrentTaskText.text = GrabProductText;
+                Debug.Log("Grabbed Box");
+            }
+        }
+
+        private void GrabProduct()
+        {
+            if (CurrentTask == Task.GrabProduct)
+            {
+                CurrentTask = Task.DepositBox;
+                CurrentTaskText.text = DepositBoxText;
+                Debug.Log("Grabbed Product");
+            }
+        }
+
+        private void DepositBox()
+        {
+            if (CurrentTask == Task.DepositBox)
+            {
+                CurrentTask = Task.GrabBox;
+                CurrentTaskText.text = GrabBoxText;
+                BoxesDepositedCount.Value++;
+                Debug.Log($"Deposited Box #{BoxesDepositedCount.Value}");
+            }
+        }
+    }
+}
