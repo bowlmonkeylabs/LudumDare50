6d3c167d3df9e08db7575bbcccbc780f655b9a74
Max Lepkowski <mlepkows@stevens.edu>
2022-04-04 02:02:48 -0400

    Add FloatThresholdEvents.cs

---
+++ Assets/Scripts/FloatThresholdEvents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using BML.ScriptableObjectCore.Scripts.Variables;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace DefaultNamespace
+{
+    public class FloatThresholdEvents : MonoBehaviour
+    {
+        public class ThresholdEvent
+        {
+            public float Threshold;
+            public UnityEvent Event;
+        }
+
+        [SerializeField] private FloatReference value;
+        [SerializeField] private List<ThresholdEvent> thresholdEvents;
+
+        private float prevValue;
+
+        private void Awake()
+        {
+            value.Subscribe(TryFireEvent);
+            prevValue = value.Value;
+        }
+
+        private void OnDestroy()
+        {
+            value.Unsubscribe(TryFireEvent);
+        }
+
+        private void TryFireEvent()
+        {
+            foreach (var thresholdEvent in thresholdEvents)
+            {
+                if (prevValue < thresholdEvent.Threshold
+                    && thresholdEvent.Threshold <= value.Value)
+                {
+                    thresholdEvent.Event.Invoke();
+                }
+            }
+
+            prevValue = value.Value;
+        }
+    }
+}
